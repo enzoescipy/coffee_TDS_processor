0d317c405c05b5d81eeea2ff9a274089c1d7ad03
SciKa <enzoescipy@gmail.com>
2022-04-16 17:37:30 +0900

    first commit. progress for notion 5 day note.

---
+++ 1st_statistic_and_criticism.py
@@ -0,0 +1,258 @@
+import sympy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.interpolate as spline
+from pprint import pprint
+
+#data refinement
+class Dataset:
+    def __init__(self, datalist):
+        expr = self._refine(datalist)
+        if expr == -1 :
+            self.err = "ERROR!"
+            return 
+        self.id = expr["id"]
+        self.expr_num, self.trial_num, self.repeat_num = map(int,list(self.id))
+        self.vapo_conc = ((expr["bowl_after"] - expr["bowl_blank"]) / (expr["bowl_before"] - expr["bowl_blank"]))*100
+        self.dillution_rate = expr["source_mass"] / (expr["water_mass"] + expr["source_mass"])
+        self.TDS = expr["mixed_TDS"] / 10000
+        self.EC = expr["mixed_EC"] / 10000
+        self.Brix = expr["mixed_Brix"]
+        self.temp = expr["mixed_temp"]
+
+    def _refine(self, datalist):
+        namelist = ["id","X","source_mass","X","water_mass","X","water_temp","mixed_temp","mixed_TDS","mixed_EC","mixed_Brix","bowl_blank","bowl_before","bowl_after"]
+        datadict = {}
+        try:
+            datadict = dict(zip(namelist, datalist))
+            while True:
+                checkif = datadict.pop("X",None)
+                if checkif == None:
+                    break
+            numberized_dict = {}
+            for key, value in datadict.items():
+                if key == "id":
+                    numberized_dict[key] = value
+                    continue
+                numberized_dict[key] = float(value)
+            return numberized_dict
+        except Exception as exp:
+            print(exp)
+            return -1
+        
+
+datasets = []
+
+with open("DATA.csv", "r") as f:
+    while True:
+        line = f.readline()
+        lineparsed = line.strip("\n").split(",")
+        if line == "":
+            break
+        datasets.append(Dataset(lineparsed))
+
+
+expr1 = [[],[],[]]
+expr2 = [[],[],[]]
+
+for dataset in datasets:
+    if dataset.expr_num == 1:
+        expr1[dataset.repeat_num - 1].append(dataset)
+    elif dataset.expr_num == 2:
+        expr2[dataset.repeat_num - 1].append(dataset)
+
+
+#expr2 : deviation calculate
+expr2_SD = []
+for trial in expr2:
+    print("2!")
+    avr_vapoConc = 0
+    avr_TDS = 0
+    avr_EC = 0
+    avr_Brix = 0
+    for data in trial:
+        avr_vapoConc += data.vapo_conc
+        avr_TDS += data.TDS
+        avr_EC += data.EC
+        avr_Brix += data.Brix
+    avr_vapoConc /= len(trial) 
+    avr_TDS /= len(trial) 
+    avr_EC /= len(trial) 
+    avr_Brix /= len(trial) 
+
+    sD_vapoConc = 0
+    sD_TDS = 0
+    sD_EC = 0
+    sD_Brix = 0
+    for data in trial:
+        sD_vapoConc += (avr_vapoConc - data.vapo_conc)**2
+        sD_TDS += (avr_TDS - data.TDS)**2
+        sD_EC += (avr_EC - data.EC)**2
+        sD_Brix += (avr_Brix - data.Brix)**2
+    
+    sD_vapoConc = sD_vapoConc**(1/2)
+    sD_TDS = sD_TDS**(1/2)
+    sD_EC = sD_EC**(1/2)
+    sD_Brix = sD_Brix**(1/2)
+    expr2_SD.append([(sD_vapoConc,avr_vapoConc),
+                    (sD_TDS,avr_TDS),
+                    (sD_EC,avr_EC),
+                    (sD_Brix,avr_Brix)])
+
+#expr1 : linearlity calculate
+expr1_lty = []
+for trial in expr1:
+    print("1!")
+    #function splinizing
+    basicfunc_dict = {"x_dilluRate":[],"y_vapoConc":[],"y_TDS":[],"y_EC":[],"y_Brix":[]}
+
+    for data in trial:
+        basicfunc_dict["x_dilluRate"].append(data.dillution_rate)
+        basicfunc_dict["y_vapoConc"].append(data.vapo_conc)
+        basicfunc_dict["y_TDS"].append(data.TDS)
+        basicfunc_dict["y_EC"].append(data.EC)
+        basicfunc_dict["y_Brix"].append(data.Brix)
+
+    np_basicfunc_dict = {}
+    for key, value in basicfunc_dict.items():
+        np_basicfunc_dict[key] = np.array(list(reversed(basicfunc_dict[key])))
+    np_spliedPrefunc_dict = {
+        "vapoConc":spline.interp1d(basicfunc_dict["x_dilluRate"],basicfunc_dict["y_vapoConc"],kind='quadratic'),
+        "TDS":spline.interp1d(basicfunc_dict["x_dilluRate"],basicfunc_dict["y_TDS"],kind='quadratic'),
+        "EC":spline.interp1d(basicfunc_dict["x_dilluRate"],basicfunc_dict["y_EC"],kind='quadratic'),
+        "Brix":spline.interp1d(basicfunc_dict["x_dilluRate"],basicfunc_dict["y_Brix"],kind='quadratic')
+    }
+
+    np_spliedfunc_dict = {}
+
+    dilluRate_linspace = np.linspace(basicfunc_dict["x_dilluRate"][0],basicfunc_dict["x_dilluRate"][-1],102)
+    for key,value in np_spliedPrefunc_dict.items():
+        np_spliedfunc_dict[key] = np_spliedPrefunc_dict[key](dilluRate_linspace)
+
+    #function double differential
+
+    def derivative(index, xarr, yarr):
+        xb = xarr[index-1]
+        xf = xarr[index+1]
+        yb = yarr[index-1]
+        yf = yarr[index+1]
+        return (yf-yb)/(xf-xb)
+
+    np_diff1Func_dict = {}
+    np_diff1avg_dict = {}
+
+    for key, value in np_spliedfunc_dict.items():
+        target_funcArr = value
+        result = []
+        diff_avg = 0
+        for i in range(1, len(target_funcArr) - 1):
+            output = derivative(i,dilluRate_linspace,target_funcArr)
+            diff_avg += output
+            result.append(output)
+        diff_avg /= (len(target_funcArr) - 1)
+        np_diff1avg_dict[key] = diff_avg
+        np_diff1Func_dict[key] = np.array(result)
+    
+    dilluRate_Diff1_linspace = []
+    for i in range(1, len(dilluRate_linspace)-1):
+        dilluRate_Diff1_linspace.append(dilluRate_linspace[i])
+    dilluRate_Diff1_linspace = np.array(dilluRate_Diff1_linspace)
+
+
+
+
+    np_diff2Func_dict = {}
+
+    for key, value in np_diff1Func_dict.items():
+        target_funcArr = value
+        result = []
+        for i in range(1, len(target_funcArr) - 1):
+            output = derivative(i,dilluRate_Diff1_linspace,target_funcArr)
+            result.append(output)
+        np_diff2Func_dict[key] = np.array(result)
+    
+    dilluRate_Diff2_linspace = []
+    for i in range(1, len(target_funcArr) - 1):
+        dilluRate_Diff2_linspace.append(dilluRate_Diff1_linspace[i])
+    dilluRate_Diff2_linspace = np.array(dilluRate_Diff2_linspace)
+
+    
+    #function integral calculate
+
+    def fxdx(index, xarr, yarr):
+        dx = xarr[index+1] - xarr[index]
+        dy = (yarr[index+1] + yarr[index]) / 2
+        return dx * dy
+
+    diff2_integral_dict = {}
+    for key, value in np_diff2Func_dict.items():
+        target_funcArr = value
+        integral_sum = 0
+        for i in range(len(target_funcArr) - 1):
+            integral_sum += np.abs(fxdx(i,dilluRate_Diff2_linspace,target_funcArr))
+        
+        diff2_integral_dict[key] = integral_sum
+
+    expr1_lty.append([(diff2_integral_dict["vapoConc"],np_diff1avg_dict["vapoConc"]), 
+                    (diff2_integral_dict["TDS"],np_diff1avg_dict["TDS"]), 
+                    (diff2_integral_dict["EC"],np_diff1avg_dict["EC"]), 
+                    (diff2_integral_dict["Brix"],np_diff1avg_dict["Brix"])
+                    ])
+    
+    '''
+    plt.plot(dilluRate_linspace, np_spliedfunc_dict["vapoConc"],'r')
+    plt.plot(dilluRate_linspace, np_spliedfunc_dict["TDS"],'g')
+    plt.plot(dilluRate_linspace, np_spliedfunc_dict["Brix"],'b')
+
+    plt.plot(dilluRate_Diff1_linspace, np_diff1Func_dict["vapoConc"],color='r', linestyle='--')
+    plt.plot(dilluRate_Diff1_linspace, np_diff1Func_dict["TDS"],color='g', linestyle='--')
+    plt.plot(dilluRate_Diff1_linspace, np_diff1Func_dict["Brix"],color='b', linestyle='--')
+
+    plt.show()
+    '''
+print("vapor, TDS, EC, Brix")
+print("1:concentration linearlity")
+pprint(expr1_lty)
+print("\n")
+print("2:temp deviation")
+pprint(expr2_SD)
+
+#point distribution graph
+##organize data
+expr1_distributy_x = []
+expr1_distributy_y = []
+for chart in expr1_lty:
+    for spot in chart:
+        expr1_distributy_x.append(spot[1]) # size of gradient
+        expr1_distributy_y.append(spot[0]) # concentration linearlity
+
+expr2_distributy_x = []
+expr2_distributy_y = []
+for chart in expr2_SD:
+    for spot in chart:
+        expr2_distributy_x.append(spot[1]) # size of value
+        expr2_distributy_y.append(spot[0]) # S.D of value in temp variation
+
+
+expr1_distributy_x = np.array(expr1_distributy_x)
+expr1_distributy_y = np.array(expr1_distributy_y)
+expr2_distributy_x = np.array(expr2_distributy_x)
+expr2_distributy_y = np.array(expr2_distributy_y)
+
+plt.subplot(2,1,1)
+plt.scatter(expr1_distributy_x,expr1_distributy_y)
+
+plt.subplot(2,1,2)
+plt.scatter(expr2_distributy_x,expr2_distributy_y)
+
+plt.show()
+
+#relation constant calculation
+
+
+
+
+    
+
+
+
